3db960668cc7bd6a67872223bb91a1c0ab58e7dc
stevenpslade <stevenpslade@gmail.com>
2018-07-02 13:37:17 -0700

    additional npm setup

--- contracts/HodlersDilemma.sol
+++ contracts/HodlersDilemma.sol
@@ -3,6 +3,19 @@ pragma solidity ^0.4.24;
 contract HodlersDilemma {
   address public owner;
 
+  struct Game {
+    address player1;
+    address player2;
+    bytes32 player1Commitment;
+    bytes32 player2Commitment;
+    uint256 expiration;
+    bool complete;
+  }
+
+  Game[] public games;
+
+  mapping(address => uint256) playerToGame;
+
   constructor() public {
     owner = msg.sender;
   }
