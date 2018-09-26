f8c3578015bd57706e8e61c75c20466861c1f24b
stevenpslade <stevenpslade@gmail.com>
2018-09-25 21:40:33 -0700

    fleshing out communicating with smart contract with drizzle

--- client/src/Game.js
+++ client/src/Game.js
@@ -1,15 +1,55 @@
 import React from 'react';
 
 class Game extends React.Component {
+  state = { dataKey: null, stackId: null };
+
   componentDidMount() {
     const { drizzle, drizzleState } = this.props;
-    console.log(drizzle);
-    console.log(drizzleState);
-  }
+    const contract = drizzle.contracts.HodlersDilemma;
+
+    const dataKey = contract.methods['gameWager'].cacheCall();
+
+    this.setState({ dataKey });
+  };
+
+  startGame = (choice, nonce, wager) => {
+    const { drizzle, drizzleState } = this.props;
+    const contract = drizzle.contracts.HodlersDilemma;
+
+    const commitment = drizzle.web3.utils.keccak256(choice, nonce);
+
+    const stackId = contract.methods['startGame'].cacheSend(commitment, {
+      from: drizzleState.accounts[0],
+      value: drizzle.web3.utils.toWei(wager, 'ether')
+    });
+
+    this.setState({ stackId });
+  };
+
+  getTxStatus = () => {
+    // get the transaction states from the drizzle state
+    const { transactions, transactionStack } = this.props.drizzleState;
+
+    // get the transaction hash using our saved `stackId`
+    const txHash = transactionStack[this.state.stackId];
+
+    // if transaction hash does not exist, don't display anything
+    if (!txHash) return null;
+
+    // otherwise, return the transaction status
+    return `Transaction status: ${transactions[txHash].status}`;
+  };
 
   render() {
-    return <div>Game Component</div>;
-  }
+    const { HodlersDilemma } = this.props.drizzleState.contracts;
+    const web3 = this.props.drizzle.web3;
+
+    const gameWager = HodlersDilemma.gameWager[this.state.dataKey];
+
+    // TODO: add form for starting game with choice and nonce
+
+    return <div>Game Wager: {gameWager && web3.utils.fromWei(gameWager.value, 'ether')}</div>;
+  };
 }
 
 export default Game;
